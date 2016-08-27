2ae90135decf918a810f45a7d90038128a097bf6
Aleksey Kolchanov <kolchanov.aleksey@gmail.com>
2016-08-27 13:56:42 +0500

    Initial project setup

--- TwittY/Classes/Modules/List/User Interface/View/TweetListViewController.swift
+++ TwittY/Classes/Modules/List/User Interface/View/TweetListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TweetListViewController.swift
 //  TwittY
 //
 //  Created by Aleksei Kolchanov on 27/08/16.
@@ -8,18 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TweetListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        //TODO: Implement data source cleaning on memory warning
     }
 
 
 }
-
